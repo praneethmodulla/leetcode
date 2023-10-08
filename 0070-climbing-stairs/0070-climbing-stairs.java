ae9262ed8fab6f37157521415736606a3a3944cd
Medha Praneeth Reddy Modulla <141873665+praneethmodulla@users.noreply.github.com>
2023-10-08 00:35:36 -0400

    Time: 0 ms (100%), Space: 39.1 MB (45.26%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -1,17 +1,14 @@
 class Solution {
+    HashMap<Integer, Integer> mp = new HashMap<>();
     public int climbStairs(int n) {
-        if(n <= 1){
+        if(n <= 2){
             return n;
         }
 
-        int[] dp = new int[n + 1];
-        dp[1] = 1;
-        dp[2] = 2;
-
-        for(int i = 3; i <= n; i++){
-            dp[i] = dp[i - 1] + dp[i - 2];
+        if(!mp.containsKey(n)){
+            mp.put(n, climbStairs(n - 1) + climbStairs(n - 2));
         }
 
-        return dp[n];
+        return mp.get(n);
     }
 }
